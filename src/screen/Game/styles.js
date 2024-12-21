82fa5721e58be5552a4461d533b2b579df607e7e
masatidhira <masatidhira@gmail.com>
2024-12-22 01:32:36 +0700

    add: styles for Game component

---
+++ src/screen/Game/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  resetButton: {
+    backgroundColor: "#000",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  resetButtonText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
