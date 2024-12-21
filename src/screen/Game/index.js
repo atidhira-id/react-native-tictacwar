dfd91db3114f27f4fe64eee989462e0a28f63b57
masatidhira <masatidhira@gmail.com>
2024-12-22 01:32:05 +0700

    refactor: move game logic to hooks folder and styles to separate file

---
+++ src/screen/Game/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import styles from "./styles";
+import useTicTacWar from "../../hooks/useTicTacWar";
+import Board from "../Board";
+
+const Game = () => {
+  const { board, player, winner, isTie, handlePress, handleReset } =
+    useTicTacWar();
+
+  if (winner) {
+    Alert.alert(`Player ${winner} won!!!`, "", [
+      { text: "OK", onPress: handleReset },
+    ]);
+  } else if (isTie) {
+    Alert.alert(`It\'s a tie!!!`, "", [{ text: "OK", onPress: handleReset }]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>TicTacWar</Text>
+      <Board board={board} onPress={handlePress} />
+      <TouchableOpacity style={styles.resetButton} onPress={handleReset}>
+        <Text style={styles.resetButtonText}>Reset</Text>
+      </TouchableOpacity>
+      <Text>player: {player}</Text>
+      <Text>winner: {winner}</Text>
+      <Text>isTie: {isTie ? "YES" : "NO"}</Text>
+    </View>
+  );
+};
+
+export default Game;
