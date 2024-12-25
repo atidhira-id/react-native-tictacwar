5ce1664e0c38af21f78dab7feff2a75c451834a7
masatidhira <masatidhira@gmail.com>
2024-12-26 03:24:46 +0700

    add: simple screen for Instructions

---
+++ screen/Instructions/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+const Instructions = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Instructions</Text>
+    </View>
+  );
+};
+
+export default Instructions;
