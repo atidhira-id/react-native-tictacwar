e595100dda5e5a2cbec0d47a2f5a7f9aacd6079c
masatidhira <masatidhira@gmail.com>
2024-12-26 03:23:28 +0700

    add: simple credits screen

---
+++ screen/Credits/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+const Credits = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Credits</Text>
+    </View>
+  );
+};
+
+export default Credits;
