5c9bbbbff001804e86fd38bac1cad1986dd149db
masatidhira <masatidhira@gmail.com>
2024-12-31 01:30:08 +0700

    update: add credits content

--- screen/Credits/index.js
+++ screen/Credits/index.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 import styles from "./styles";
 
 const Credits = () => {
   return (
-    <View style={styles.container}>
-      <Text>Credits</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Credits</Text>
+
+      <Text style={styles.sectionHeader}>Dibuat Oleh:</Text>
+      <Text style={styles.text}>Atidhira Habibillah</Text>
+      <Text style={styles.text}>Role: Developer</Text>
+
+      <Text style={styles.sectionHeader}>Support Team:</Text>
+      <Text style={styles.text}>- Muh. Attokillah S.H</Text>
+      <Text style={styles.text}>- Muh. Farros Putra</Text>
+      <Text style={styles.text}>- Adillah Giyats</Text>
+      <Text style={styles.text}>- Danang</Text>
+
+      <Text style={styles.sectionHeader}>Dibuat dengan:</Text>
+      <Text style={styles.text}>- React Native</Text>
+      <Text style={styles.text}>- React Navigation</Text>
+
+      <Text style={styles.sectionHeader}>Special Thanks:</Text>
+      <Text style={styles.text}>
+        - Tim Laboran yang sudah meminjamkan komputer untuk membuat project ini.
+      </Text>
+    </ScrollView>
   );
 };
 
