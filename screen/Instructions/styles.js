345bfa581d0d2b00eb771de409d57d6af33f37e2
masatidhira <masatidhira@gmail.com>
2025-01-05 00:00:18 +0700

    update: use themes for colors

--- screen/Instructions/styles.js
+++ screen/Instructions/styles.js
@@ -1,35 +1,22 @@
 import { StyleSheet } from "react-native";
+import { useTheme } from "../../styles/themes";
 
+const themes = useTheme();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "#f9f9f9",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
+    backgroundColor: themes.color.appBackground,
   },
   instruction: {
+    color: themes.color.text,
     fontSize: 16,
     marginVertical: 5,
     lineHeight: 24,
   },
   button: {
     marginTop: 30,
-    backgroundColor: "#007BFF",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
     alignSelf: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    textAlign: "center",
-    fontWeight: "bold",
   },
 });
 
