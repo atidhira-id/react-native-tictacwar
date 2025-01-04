1c7c48045a3fed96041b0a86b01cf2a3a075ff23
masatidhira <masatidhira@gmail.com>
2025-01-04 23:57:52 +0700

    update: erase unuse styling and add gameStatus styling

--- screen/Game/styles.js
+++ screen/Game/styles.js
@@ -1,26 +1,13 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  gameStatus: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  resetButton: {
-    backgroundColor: "#000",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  resetButtonText: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold",
+    alignSelf: "stretch",
+    marginHorizontal: "auto",
+    width: 300,
   },
 });
 
